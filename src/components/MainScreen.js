fa6a29390fdb4642838bc2635c70e52549ae4e89
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-07-20 09:58:01 +0700

    first box appear

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Text, View} from 'react-native'
+import {Text, View, TouchableOpacity} from 'react-native'
 class MainScreen extends Component {
   constructor(props) {
     super(props);
@@ -7,9 +7,26 @@ class MainScreen extends Component {
   }
   render() {
     return (
-      <Text> ini halaman main screen</Text>
+      <View style={styles.container}>
+        <View style={styles.row1}>
+          <TouchableOpacity style={styles.box1}/>
+        </View>
+      </View>
     );
   }
 }
 
+const styles = {
+  container : {
+
+  },
+  row1 : {
+    
+  },
+  box1: {
+    borderWidth: 1,
+    height: 50,
+    width: 50
+  }
+}
 export default MainScreen;
